e4ac81b3cb47d8da7e6410950c39154cdc785825
gerardroyo <g.royo1999@gmail.com>
2025-02-08 20:09:36 +0100

    al dia con udemy 2/2

--- src/test/java/pages/PaginaPrincipal.java
+++ src/test/java/pages/PaginaPrincipal.java
@@ -2,7 +2,7 @@ package pages;
 
 public class PaginaPrincipal extends BasePage {
 
-    //private String botonAcceder = "//button[normalize-space()='Accede ya']";
+    private String botonAcceder = "//a[@href='https://www.freerangetesters.com/nivel-inicial-fundamentos-del-testing']";
 
     public PaginaPrincipal(){
         super(driver);
