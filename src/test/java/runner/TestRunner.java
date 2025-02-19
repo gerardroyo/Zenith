1c20f79687c26a2295496556940e503c8a037f0c
gerardroyo <g.royo1999@gmail.com>
2025-02-19 23:50:38 +0100

    allureReports fuera y extends tambien, voy a dedicame a arreglar el comando git test que no me ejecuta nada, cosorro

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,15 +10,17 @@ import pages.BasePage;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src\\test\\resources\\features", // Directorio de nuestros archivos .feature
         glue = "steps", // Paquete donde tenemos nuestras clases definiendo los steps escritos en el feature file
-        plugin = { "pretty", "html:target/cucumber-reports", "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm" },
+        plugin = { "pretty", "html:target/cucumber-reports" }, // Plugin para generar reportes
+        //monochrome = true, // Hace que los reportes sean más legibles
         tags = "@Navegacion")
         // comando para ejecutar tags en concreto en la terminal:
         // PowerShell: gradle test -D "cucumber.filter.tags=@Plans"
         // Bash: ./gradlew test -Dcucumber.filter.tags="@Planss"
 
 public class TestRunner {
+    
     @AfterClass
-    public static void cleanDriver() {
+    public static void tearDown() {
         BasePage.closeBrowser();
     }
 }
