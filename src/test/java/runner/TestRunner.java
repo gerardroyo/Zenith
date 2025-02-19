84cdbd0fdd6bb111301a44b283487aae5718d58a
gerardroyo <g.royo1999@gmail.com>
2025-02-20 00:34:37 +0100

    no se que le pasa que me genera reportes pero no me ejecuta las pruebas, fin de dia, 19 de febrero de 2025

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,17 +8,15 @@ import io.cucumber.junit.CucumberOptions;
 import pages.BasePage;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources\\features", // Directorio de nuestros archivos .feature
+@CucumberOptions(features = "src\\test\\resources\\features", // Directorio de nuestros archivos .feature
         glue = "steps", // Paquete donde tenemos nuestras clases definiendo los steps escritos en el feature file
-        plugin = { "pretty", "html:target/cucumber-reports" }, // Plugin para generar reportes
-        //monochrome = true, // Hace que los reportes sean más legibles
-        tags = "@Navegacion")
+        plugin = {"pretty", "html:target/cucumber-reports"}) // Plugin para generar reportes
+        //tags = "@Navegacion")
         // comando para ejecutar tags en concreto en la terminal:
         // PowerShell: gradle test -D "cucumber.filter.tags=@Plans"
         // Bash: ./gradlew test -Dcucumber.filter.tags="@Planss"
 
-public class TestRunner {
-    
+public class TestRunner { 
     @AfterClass
     public static void tearDown() {
         BasePage.closeBrowser();
--- src/test/resources/features/FreeRangeNavegation.feature
+++ src/test/resources/features/FreeRangeNavegation.feature
@@ -1,8 +1,7 @@
-@Navegacion
 Feature: Navegación
     Para ver las paginas dentro de FRT
     Sin haber logueado
-    Puedo hacer click en los linkss
+    Puedo hacer click en los links
 
     Background: Estoy en la web de Free Range Testers sin la sesión iniciada
         Given Navegar a www.freerangtesters.com
@@ -18,12 +17,12 @@ Feature: Navegación
             | Udemy     |
             | Academia  |
 
-    @Cursos
+    
     Scenario: Los cursos se muestran de la forma correcta a los potenciales clientes
         When Voy a Cursos usando la barra de navegacion
         And Seleccionar Introduccion al Testing y comprar
 
-    @Planes @Cursos
+    
     Scenario: Los susarios puedes seleccionar un plan cuando estan registrados
         When El usuario selecciona Recursos y navega a Automation Sandbox
         Then El sistema puede validar las opciones del primer dropdown de la apgina Automation Sandbox
